ddceca6b79549f71a109312b668dd697dda937ab
basila1 <basila_n@outlook.com>
2018-03-10 23:14:06 -0800

    async API call completed

--- react-weather/src/App.js
+++ react-weather/src/App.js
@@ -3,13 +3,26 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = "9cdd8cd57fae7ff93e7d652fc42f6648";
+
 
 class App extends React.Component {
+  state = {
+    
+  }
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather = {this.getWeather} />
         <Weather />
       </div>
     );
